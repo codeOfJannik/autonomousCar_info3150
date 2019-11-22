bcb8b5111649177f427bf3d2930d9c2d6d5399bd
Jannik Schlemmer <js329@hdm-stuttgart.de>
2019-11-21 16:52:34 -0800

    changed from compass class to compass script

--- Library/scripts/compassClass.py
+++ Library/scripts/compassClass.py
@@ -1,5 +1,5 @@
 import sys, getopt
-from thread import start_new_thread
+from motor import Motor
 
 sys.path.append('.')
 import RTIMU
@@ -10,15 +10,48 @@ import operator
 import socket
 import os
 
-class Compass:
-
+def compass_script():
     IMU_IP = "127.0.0.2"
     IMU_PORT = 5005
 
     MON_IP = "127.0.0.5"
     MON_PORT = 5005
 
-    __SETTINGS_FILE = "RTIMULib"
+    SETTINGS_FILE = "RTIMULib"
+
+    s = RTIMU.Settings(SETTINGS_FILE)
+    imu = RTIMU.RTIMU(s)
+    motor = Motor()
+
+    # offsets
+    yawoff = 0.0
+    pitchoff = 0.0
+    rolloff = 0.0
+
+    # timers
+    t_print = time.time()
+    t_damp = time.time()
+    t_fail = time.time()
+    t_fail_timer = 0.0
+    t_shutdown = 0
+
+    if (not imu.IMUInit()):
+        hack = time.time()
+        imu_sentence = "$IIXDR,IMU_FAILED_TO_INITIALIZE*7C"
+        if (hack - t_print) > 1.0:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.sendto(imu_sentence, (IMU_IP, IMU_PORT))
+            t_print = hack
+            t_shutdown += 1
+            if t_shutdown > 9:
+                sys.exit(1)
+
+    imu.setSlerpPower(0.02)
+    imu.setGyroEnable(True)
+    imu.setAccelEnable(True)
+    imu.setCompassEnable(True)
+
+    poll_interval = imu.IMUGetPollInterval()
 
     # data variables
     roll = 0.0
@@ -30,9 +63,9 @@ class Compass:
     yawrate = 0.0
 
     # magnetic deviation
-    # f = open('mag', 'r')
+    #f = open('mag', 'r')
     magnetic_deviation = 0.0
-    # f.close()
+    #f.close()
 
     # dampening variables
     t_one = 0
@@ -44,81 +77,73 @@ class Compass:
     heading_cos_run = [0] * 30
     heading_sin_run = [0] * 30
 
-    # timers
-    t_print = time.time()
-    t_damp = time.time()
-    t_fail = time.time()
-    t_fail_timer = 0.0
-    t_shutdown = 0
+    while True:
 
-    def __init__(self):
-        s = RTIMU.Settings(self.__SETTINGS_FILE)
-        self.imu = RTIMU.RTIMU(s)
+        hack = time.time()
 
-        self.imu.setSlerpPower(0.02)
-        self.imu.setGyroEnable(True)
-        self.imu.setAccelEnable(True)
-        self.imu.setCompassEnable(True)
+        # if it's been longer than 5 seconds since last print
+        if (hack - t_damp) > 5.0:
 
-        self.poll_interval = self.imu.IMUGetPollInterval()
+            if (hack - t_fail) > 1.0:
+                t_one = 0
+                t_three = 0
+                roll_total = 0.0
+                roll_run = [0] * 10
+                heading_cos_total = 0.0
+                heading_sin_total = 0.0
+                heading_cos_run = [0] * 30
+                heading_sin_run = [0] * 30
+                t_fail_timer += 1
+                imu_sentence = "IIXDR,IMU_FAIL," + str(round(t_fail_timer / 60, 1))
+                cs = format(reduce(operator.xor, map(ord, imu_sentence), 0), 'X')
+                if len(cs) == 1:
+                    cs = "0" + cs
+                imu_sentence = "$" + imu_sentence + "*" + cs
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                sock.sendto(imu_sentence, (IMU_IP, IMU_PORT))
+                t_fail = hack
+                t_shutdown += 1
 
-    def measurment_function(self):
-        while True:
+        if imu.IMURead():
+            data = imu.getIMUData()
+            fusionPose = data["fusionPose"]
+            Gyro = data["gyro"]
+            t_fail_timer = 0.0
 
-            self.hack = time.time()
+            if (hack - t_damp) > .1:
+                roll = round(math.degrees(fusionPose[0]) - rolloff, 1)
+                pitch = round(math.degrees(fusionPose[1]) - pitchoff, 1)
+                yaw = round(math.degrees(fusionPose[2]) - yawoff, 1)
+                rollrate = round(math.degrees(Gyro[0]), 1)
+                pitchrate = round(math.degrees(Gyro[1]), 1)
+                yawrate = round(math.degrees(Gyro[2]), 1)
+                if yaw < 0.1:
+                    yaw = yaw + 360
+                if yaw > 360:
+                    yaw = yaw - 360
 
-            if self.imu.IMURead():
-                data = self.imu.getIMUData()
-                fusionPose = data["fusionPose"]
-                Gyro = data["gyro"]
-                self.t_fail_timer = 0.0
+                # Dampening functions
+                roll_total = roll_total - roll_run[t_one]
+                roll_run[t_one] = roll
+                roll_total = roll_total + roll_run[t_one]
+                roll = round(roll_total / 10, 1)
+                heading_cos_total = heading_cos_total - heading_cos_run[t_three]
+                heading_sin_total = heading_sin_total - heading_sin_run[t_three]
+                heading_cos_run[t_three] = math.cos(math.radians(yaw))
+                heading_sin_run[t_three] = math.sin(math.radians(yaw))
+                heading_cos_total = heading_cos_total + heading_cos_run[t_three]
+                heading_sin_total = heading_sin_total + heading_sin_run[t_three]
+                yaw = round(math.degrees(math.atan2(heading_sin_total / 30, heading_cos_total / 30)), 1)
+                if yaw < 0.1:
+                    yaw = yaw + 360.0
 
-                if (self.hack - self.t_damp) > .1:
-                    self.roll = round(math.degrees(fusionPose[0]) - self.rolloff, 1)
-                    self.pitch = round(math.degrees(fusionPose[1]) - self.pitchoff, 1)
-                    self.yaw = round(math.degrees(fusionPose[2]) - self.yawoff, 1)
-                    self.rollrate = round(math.degrees(Gyro[0]), 1)
-                    self.pitchrate = round(math.degrees(Gyro[1]), 1)
-                    self.yawrate = round(math.degrees(Gyro[2]), 1)
-                    if self.yaw < 0.1:
-                        self.yaw = self.yaw + 360
-                    if self.yaw > 360:
-                        self.yaw = self.yaw - 360
+                # yaw is magnetic heading, convert to true heading
+                heading = yaw - magnetic_deviation
+                if heading < 0.1:
+                    heading = heading + 360
+                if heading > 360:
+                    heading = heading - 360
 
-                    # Dampening functions
-                    self.roll_total = self.roll_total - self.roll_run[self.t_one]
-                    self.roll_run[self.t_one] = self.roll
-                    self.roll_total = self.roll_total + self.roll_run[self.t_one]
-                    self.roll = round(self.roll_total / 10, 1)
-                    self.heading_cos_total = self.heading_cos_total - self.heading_cos_run[self.t_three]
-                    self.heading_sin_total = self.heading_sin_total - self.heading_sin_run[self.t_three]
-                    self.heading_cos_run[self.t_three] = math.cos(math.radians(self.yaw))
-                    self.heading_sin_run[self.t_three] = math.sin(math.radians(self.yaw))
-                    self.heading_cos_total = self.heading_cos_total + self.heading_cos_run[self.t_three]
-                    self.heading_sin_total = self.heading_sin_total + self.heading_sin_run[self.t_three]
-                    self.yaw = round(math.degrees(math.atan2(self.heading_sin_total / 30, self.heading_cos_total / 30)), 1)
-                    if self.yaw < 0.1:
-                        self.yaw = self.yaw + 360.0
+                return heading
 
-                    # yaw is magnetic heading, convert to true heading
-                    self.heading = self.yaw - self.magnetic_deviation
-                    if self.heading < 0.1:
-                        self.heading = self.heading + 360
-                    if self.heading > 360:
-                        self.heading = self.heading - 360
-
-                    print(str(heading))
-
-                    self.t_damp = self.hack
-                    self.t_one += 1
-                    if self.t_one == 10:
-                        self.t_one = 0
-                    self.t_three += 1
-                    if self.t_three == 30:
-                        self.t_three = 0
-
-                time.sleep(self.poll_interval * 1.0 / 1000.0)
-
-    def start_measurment(self):
-        start_new_thread(self.measurment_function())
-
+            time.sleep(poll_interval * 1.0 / 1000.0)
