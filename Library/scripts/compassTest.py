353ca2a2e03a326557ca0d85a82d4a0b8d398a23
Jannik Schlemmer <js329@hdm-stuttgart.de>
2019-11-21 17:01:39 -0800

    renamed compassClass to compassScript

--- Library/scripts/compassTest.py
+++ Library/scripts/compassTest.py
@@ -1,4 +1,4 @@
-from compassClass import compass_script
+from compassScript import compass_script
 from motor import Motor
 import time
 
@@ -10,14 +10,13 @@ except KeyboardInterrupt:
     print("Quit")
 
 def driving_compass_test():
-    compass = Compass()
     motor = Motor()
 
     motor.forward()
     time.sleep(1)
     motor.stop()
 
-    currentDirection = compass.get_heading()
+    currentDirection = compass_script()
     print("current direction: " + str(currentDirection))
     finalDirection = currentDirection + 90 if currentDirection + 90 < 360 else currentDirection + 90 - 360
     print("final direction: " + str(currentDirection))
@@ -25,13 +24,13 @@ def driving_compass_test():
     try:
         motor.turnRight()
         while True:
-            currentDirection = compass.get_heading()
+            currentDirection = compass_script()
             print("current direction: " + str(currentDirection))
 
             if currentDirection >= finalDirection:
                 motor.stop()
                 motor.__del__()
-                currentDirection = compass.get_heading()
+                currentDirection = compass_script()
                 print("current direction: " + str(currentDirection))
                 break
     except KeyboardInterrupt:
