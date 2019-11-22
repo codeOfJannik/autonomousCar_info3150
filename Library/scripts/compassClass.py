651fee54224b9b437212e5af8f816e028411b9ac
Jannik Schlemmer <js329@hdm-stuttgart.de>
2019-11-21 16:35:19 -0800

    tried to fix compass

--- Library/scripts/compassClass.py
+++ Library/scripts/compassClass.py
@@ -12,8 +12,15 @@ import os
 
 class Compass:
 
+    IMU_IP = "127.0.0.2"
+    IMU_PORT = 5005
+
+    MON_IP = "127.0.0.5"
+    MON_PORT = 5005
+
     __SETTINGS_FILE = "RTIMULib"
 
+    # data variables
     roll = 0.0
     pitch = 0.0
     yaw = 0.0
@@ -21,8 +28,13 @@ class Compass:
     rollrate = 0.0
     pitchrate = 0.0
     yawrate = 0.0
-    magnetic_deviation = 0.0
 
+    # magnetic deviation
+    # f = open('mag', 'r')
+    magnetic_deviation = 0.0
+    # f.close()
+
+    # dampening variables
     t_one = 0
     t_three = 0
     roll_total = 0.0
@@ -31,6 +43,13 @@ class Compass:
     heading_sin_total = 0.0
     heading_cos_run = [0] * 30
     heading_sin_run = [0] * 30
+
+    # timers
+    t_print = time.time()
+    t_damp = time.time()
+    t_fail = time.time()
+    t_fail_timer = 0.0
+    t_shutdown = 0
 
     def __init__(self):
         s = RTIMU.Settings(self.__SETTINGS_FILE)
@@ -46,46 +65,59 @@ class Compass:
     def measurment_function(self):
         while True:
 
+            self.hack = time.time()
+
             if self.imu.IMURead():
                 data = self.imu.getIMUData()
                 fusionPose = data["fusionPose"]
                 Gyro = data["gyro"]
+                self.t_fail_timer = 0.0
 
-                self.roll = round(math.degrees(fusionPose[0]), 1)
-                self.pitch = round(math.degrees(fusionPose[1]), 1)
-                self.yaw = round(math.degrees(fusionPose[2]), 1)
-                self.rollrate = round(math.degrees(Gyro[0]), 1)
-                self.pitchrate = round(math.degrees(Gyro[1]), 1)
-                self.yawrate = round(math.degrees(Gyro[2]), 1)
-                if self.yaw < 0.1:
-                    self.yaw = self.yaw + 360
-                if self.yaw > 360:
-                    self.yaw = self.yaw - 360
+                if (self.hack - self.t_damp) > .1:
+                    self.roll = round(math.degrees(fusionPose[0]) - self.rolloff, 1)
+                    self.pitch = round(math.degrees(fusionPose[1]) - self.pitchoff, 1)
+                    self.yaw = round(math.degrees(fusionPose[2]) - self.yawoff, 1)
+                    self.rollrate = round(math.degrees(Gyro[0]), 1)
+                    self.pitchrate = round(math.degrees(Gyro[1]), 1)
+                    self.yawrate = round(math.degrees(Gyro[2]), 1)
+                    if self.yaw < 0.1:
+                        self.yaw = self.yaw + 360
+                    if self.yaw > 360:
+                        self.yaw = self.yaw - 360
 
-                # Dampening functions
-                self.roll_total = self.roll_total - self.roll_run[self.t_one]
-                self.roll_run[self.t_one] = self.roll
-                self.roll_total = self.roll_total + self.roll_run[self.t_one]
-                self.roll = round(self.roll_total / 10, 1)
-                self.heading_cos_total = self.heading_cos_total - self.heading_cos_run[self.t_three]
-                self.heading_sin_total = self.heading_sin_total - self.heading_sin_run[self.t_three]
-                self.heading_cos_run[self.t_three] = math.cos(math.radians(self.yaw))
-                self.heading_sin_run[self.t_three] = math.sin(math.radians(self.yaw))
-                self.heading_cos_total = self.heading_cos_total + self.heading_cos_run[self.t_three]
-                self.heading_sin_total = self.heading_sin_total + self.heading_sin_run[self.t_three]
-                self.yaw = round(math.degrees(math.atan2(self.heading_sin_total / 30, self.heading_cos_total / 30)),
-                                 1)
-                if self.yaw < 0.1:
-                    self.yaw = self.yaw + 360.0
+                    # Dampening functions
+                    self.roll_total = self.roll_total - self.roll_run[self.t_one]
+                    self.roll_run[self.t_one] = self.roll
+                    self.roll_total = self.roll_total + self.roll_run[self.t_one]
+                    self.roll = round(self.roll_total / 10, 1)
+                    self.heading_cos_total = self.heading_cos_total - self.heading_cos_run[self.t_three]
+                    self.heading_sin_total = self.heading_sin_total - self.heading_sin_run[self.t_three]
+                    self.heading_cos_run[self.t_three] = math.cos(math.radians(self.yaw))
+                    self.heading_sin_run[self.t_three] = math.sin(math.radians(self.yaw))
+                    self.heading_cos_total = self.heading_cos_total + self.heading_cos_run[self.t_three]
+                    self.heading_sin_total = self.heading_sin_total + self.heading_sin_run[self.t_three]
+                    self.yaw = round(math.degrees(math.atan2(self.heading_sin_total / 30, self.heading_cos_total / 30)), 1)
+                    if self.yaw < 0.1:
+                        self.yaw = self.yaw + 360.0
 
-                # yaw is magnetic heading, convert to true heading
-                self.heading = self.yaw - self.magnetic_deviation
-                if self.heading < 0.1:
-                    self.heading = self.heading + 360
-                if self.heading > 360:
-                    self.heading = self.heading - 360
+                    # yaw is magnetic heading, convert to true heading
+                    self.heading = self.yaw - self.magnetic_deviation
+                    if self.heading < 0.1:
+                        self.heading = self.heading + 360
+                    if self.heading > 360:
+                        self.heading = self.heading - 360
 
-            time.sleep(self.poll_interval * 1.0 / 1000.0)
+                    print(str(heading))
+
+                    self.t_damp = self.hack
+                    self.t_one += 1
+                    if self.t_one == 10:
+                        self.t_one = 0
+                    self.t_three += 1
+                    if self.t_three == 30:
+                        self.t_three = 0
+
+                time.sleep(self.poll_interval * 1.0 / 1000.0)
 
     def start_measurment(self):
         start_new_thread(self.measurment_function())
