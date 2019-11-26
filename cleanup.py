bc82c7eed6c38846759dbb31e6afe621eb44df00
Raspberry Pi <pi@info3150.com>
2019-11-25 20:31:26 -0800

    v2

---
+++ cleanup.py
@@ -0,0 +1,3 @@
+import RPi.GPIO as gpio
+
+gpio.cleanup()
--- testUltrasonic.py
+++ testUltrasonic.py
@@ -7,7 +7,7 @@ sensor = Ultrasonic()
 while True:
     try:
         distance = sensor.sense()
-        print "Distance:",distance,"cm"
+        print ("Distance:",distance,"cm")
         time.sleep(0.5)
     except KeyboardInterrupt:
         sensor.end()
