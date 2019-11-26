4b52c2eea49b5c28fc5aa091ba04feb4803c1fd0
Donat-K <32346614+Donat-K@users.noreply.github.com>
2019-11-25 18:01:40 -0800

    v1 of basic obstacle avoidance.

--- ObstacleAvoidance-Pathfinding/obstacleavoidancerobot.py
+++ ObstacleAvoidance-Pathfinding/obstacleavoidancerobot.py
@@ -5,82 +5,64 @@ import time
 #Needed to import file located in different folder???
 import sys
 sys.path.insert(1, '../Library/scripts')
-import motor
-import imu
 
-# Define GPIO For Driver motors
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(37, GPIO.OUT)
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
+from RTIMUScripts import get_heading
+from motor import Motor
+from infraredSensor import InfraredSensor
+from ultrasonic import Ultrasonic
 
-# Define GPIO for ultrasonic central
-GPIO_TRIGGER_CENTRAL = 16
-GPIO_ECHO_CENTRAL = 18
-GPIO.setup(GPIO_TRIGGER_CENTRAL, GPIO.OUT)  # Trigger > Out
-GPIO.setup(GPIO_ECHO_CENTRAL, GPIO.IN)      # Echo < In
+motor = Motor();
+frontSensor = Ultrasonic()
+leftSensor = InfraredSensor(21)
+rightSensor = InfraredSensor(20)
 
-# Define GPIO for ultrasonic Right
-GPIO_TRIGGER_RIGHT = 33
-GPIO_ECHO_RIGHT = 35
-GPIO.setup(GPIO_TRIGGER_RIGHT, GPIO.OUT)  # Trigger > Out
-GPIO.setup(GPIO_ECHO_RIGHT, GPIO.IN)      # Echo < In
-
-# Define GPIO for ultrasonic Left
-GPIO_TRIGGER_LEFT = 38
-GPIO_ECHO_LEFT = 40
-GPIO.setup(GPIO_TRIGGER_LEFT, GPIO.OUT)  # Trigger > Out
-GPIO.setup(GPIO_ECHO_LEFT, GPIO.IN)      # Echo < In
-
-
-# Functions for driving
-def goforward():
-    GPIO.output(11, True)
-    GPIO.output(15, True)
-
-
-def turnleft():
-    GPIO.output(11, True)
-    GPIO.output(15, False)
-    time.sleep(0.8)
-    GPIO.output(11, False)
-
-
-def turnright():
-    GPIO.output(15, True)
-    GPIO.output(11, False)
-    time.sleep(0.8)
-    GPIO.output(15, False)
-
-
-def gobackward():
-    GPIO.output(37, True)
-    GPIO.output(13, True)
-
-
-def stopmotors():
-    GPIO.output(15, False)
-    GPIO.output(11, False)
-    GPIO.output(37, False)
-    GPIO.output(13, False)
+# # Functions for driving
+# def goforward():
+#     GPIO.output(11, True)
+#     GPIO.output(15, True)
+#
+#
+# def turnleft():
+#     GPIO.output(11, True)
+#     GPIO.output(15, False)
+#     time.sleep(0.8)
+#     GPIO.output(11, False)
+#
+#
+# def turnright():
+#     GPIO.output(15, True)
+#     GPIO.output(11, False)
+#     time.sleep(0.8)
+#     GPIO.output(15, False)
+#
+#
+# def gobackward():
+#     GPIO.output(37, True)
+#     GPIO.output(13, True)
+#
+#
+# def stopmotors():
+#     GPIO.output(15, False)
+#     GPIO.output(11, False)
+#     GPIO.output(37, False)
+#     GPIO.output(13, False)
 
 
 # Detect front obstacle
 def frontobstacle():
 
     # Set trigger to False (Low)
-    GPIO.output(GPIO_TRIGGER_CENTRAL, False)
+    GPIO.output(frontSensor.__trig, False)
     # Allow module to settle
     time.sleep(0.2)
     # Send 10us pulse to trigger
-    GPIO.output(GPIO_TRIGGER_CENTRAL, True)
+    GPIO.output(frontSensor.__trig, True)
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER_CENTRAL, False)
+    GPIO.output(frontSensor.__trig, False)
     start = time.time()
-    while GPIO.input(GPIO_ECHO_CENTRAL) == 0:
+    while GPIO.input(frontSensor.__echo) == 0:
         start = time.time()
-    while GPIO.input(GPIO_ECHO_CENTRAL) == 1:
+    while GPIO.input(frontSensor.__echo) == 1:
         stop = time.time()
     # Calculate pulse length
     elapsed = stop - start
@@ -91,90 +73,72 @@ def frontobstacle():
     return distance
 
 def rightobstacle():
-    # Set trigger to False (Low)
-    GPIO.output(GPIO_TRIGGER_RIGHT, False)
-    # Allow module to settle
-    time.sleep(0.2)
-    # Send 10us pulse to trigger
-    GPIO.output(GPIO_TRIGGER_RIGHT, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER_RIGHT, False)
-    start = time.time()
-    while GPIO.input(GPIO_ECHO_RIGHT) == 0:
-        start = time.time()
-    while GPIO.input(GPIO_ECHO_RIGHT) == 1:
-        stop = time.time()
-    # Calculate pulse length
-    elapsed = stop - start
-    # Distance pulse travelled in that time is time
-    # Multiplied by the speed of sound (cm/s)
-    distance = elapsed * 34000 / 2  # Distance of both directions so divide by 2
-    print "Right Distance : %.1f" % distance
-    return distance
+    return rightSensor.is_blocked_by_obstacle()
+
+    # # Set trigger to False (Low)
+    # GPIO.output(GPIO_TRIGGER_RIGHT, False)
+    # # Allow module to settle
+    # time.sleep(0.2)
+    # # Send 10us pulse to trigger
+    # GPIO.output(GPIO_TRIGGER_RIGHT, True)
+    # time.sleep(0.00001)
+    # GPIO.output(GPIO_TRIGGER_RIGHT, False)
+    # start = time.time()
+    # while GPIO.input(GPIO_ECHO_RIGHT) == 0:
+    #     start = time.time()
+    # while GPIO.input(GPIO_ECHO_RIGHT) == 1:
+    #     stop = time.time()
+    # # Calculate pulse length
+    # elapsed = stop - start
+    # # Distance pulse travelled in that time is time
+    # # Multiplied by the speed of sound (cm/s)
+    # distance = elapsed * 34000 / 2  # Distance of both directions so divide by 2
+    # print "Right Distance : %.1f" % distance
+    # return distance
 
 
 def leftobstacle():
-
-    # Set trigger to False (Low)
-    GPIO.output(GPIO_TRIGGER_LEFT, False)
-    # Allow module to settle
-    time.sleep(0.2)
-    # Send 10us pulse to trigger
-    GPIO.output(GPIO_TRIGGER_LEFT, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER_LEFT, False)
-    start = time.time()
-    while GPIO.input(GPIO_ECHO_LEFT) == 0:
-        start = time.time()
-    while GPIO.input(GPIO_ECHO_LEFT) == 1:
-        stop = time.time()
-    # Calculate pulse length
-    elapsed = stop - start
-    # Distance pulse travelled in that time is time
-    # Multiplied by the speed of sound (cm/s)
-    distance = elapsed * 34000 / 2  # Distance of both directions so divide by 2
-    print "Left Distance : %.1f" % distance
-    return distance
+    return leftSensor.is_blocked_by_obstacle()
 
 
 # Check front obstacle and turn right if there is an obstacle
 def checkanddrivefront():
     while frontobstacle() < 30:
-        stopmotors()
-        turnright()
-    goforward()
+        motor.stop()
+        motor.turnRight()
+    motor.forward()
 
 
 # Check right obstacle and turn left if there is an obstacle
 def checkanddriveright():
-    while rightobstacle() < 30:
-        stopmotors()
-        turnleft()
-    goforward()
+    while rightobstacle() == true:
+        motor.stop()
+        motor.turnLeft()
+    motor.forward()
 
 
 # Check left obstacle and turn right if there is an obstacle
 def checkanddriveleft():
-    while leftobstacle() < 30:
-        stopmotors()
-        turnright()
-    goforward()
+    while leftobstacle() == true:
+        motor.stop()
+        motor.turnRight()
+    motor.forward()
 
 
 # Avoid obstacles and drive forward
 def obstacleavoiddrive():
-    goforward()
+    motor.forward()
     start = time.time()
     # Drive 5 minutes
     while start > time.time() - 300:  # 300 = 60 seconds * 5
         if frontobstacle() < 30:
-            stopmotors()
+            motor.stop()
             checkanddrivefront()
-        elif rightobstacle() < 30:
-            stopmotors() 
+        elif rightobstacle() == true:
+            motor.stop()
             checkanddriveright()
-        elif leftobstacle() < 30:
-            stopmotors()
+        elif leftobstacle() == true:
+            motor.stop()
             checkanddriveleft()
     # Clear GPIOs, it will stop motors       
     cleargpios()
@@ -182,13 +146,13 @@ def obstacleavoiddrive():
 
 def cleargpios():
     print "clearing GPIO"
-    GPIO.output(37, False)
-    GPIO.output(11, False)
-    GPIO.output(13, False)
-    GPIO.output(15, False)
-    GPIO.output(16, False)
-    GPIO.output(33, False)
-    GPIO.output(38, False)    
+    GPIO.output(frontSensor.__trig, False)
+    GPIO.output(frontSensor.__echo, False)
+    # GPIO.output(23, False)
+    # GPIO.output(24, False)
+    # GPIO.output(16, False)
+    # GPIO.output(33, False)
+    # GPIO.output(38, False)
     print "All GPIOs CLEARED"
 
 
